b955b04aa5766429fc315f89438fba8f84c06ca1
manar <Manarfteis1@gmail.com>
2026-07-02 10:51:26 +0200

    Car

--- Smart parking allocation.cpp
+++ Smart parking allocation.cpp
@@ -1,1 +1,23 @@
-//
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+const int MAX_N = 50; // اكبر رقم ممكن من السيارات تقدر ادخله
+
+class Car {
+public:
+    int id;
+    int size; // حجم السيارة (1 = small, 2 = medium, 3 = large)
+
+    Car() {
+        id = 0;
+        size = 0;
+    }
+
+    Car(int id, int size) {
+        this->id = id;
+        this->size = size;
+    }
+};
